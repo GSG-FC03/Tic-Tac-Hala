f49a49e1a6c6123e8d40b591ed7b8effff2ce7de
HalaSalim77 <hala.s.salim@gmail.com>
2021-08-19 18:40:12 +0300

    create new js file - add event listener to start btn - create function to move to the next page - Create New html (Play.html) Relates #7

---
+++ js/landingPage.js
@@ -0,0 +1,9 @@
+
+//Create Function to move to the next Page 
+let startButton = document.querySelector(".Start-button");
+//Event Listener click
+startButton.addEventListener("click", moveToPlayPage); 
+//Function  
+function moveToPlayPage() {
+  window.location.href = "index.html";
+}
